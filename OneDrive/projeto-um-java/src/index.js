3bc9bf68491f6a4114ef33be1aaa9dbabd695e0b
Seu Nome <seuemail@example.com>
2024-10-08 13:39:48 -0300

    jogo da velha commit

---
+++ OneDrive/projeto-um-java/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Opcional, se você planeja usar CSS
+import App from './app';
+import './styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
